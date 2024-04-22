8fdbb1d9a20822de455aff2d9370f63e348bd391
Godwin Owusu-Boakye <kaliphaunia@gmail.com>
2024-04-22 23:44:04 +0100

    Added-files

--- s3.tf
+++ s3.tf
@@ -1,10 +1,22 @@
 #Creating S3 
 resource "aws_s3_bucket" "Project-S3" {
-  bucket = "migod-s3"
-  
-  
+  bucket = var.bucket_name
 
-  tags = {
-    Name  = "Project-S3"
+  versioning {
+    enabled = true
   }
+}
+
+resource "aws_s3_bucket_acl" "Project_acl" {
+  bucket = aws_s3_bucket.Project-S3.id
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_public_access_block" "Proj" {
+  bucket = aws_s3_bucket.Project-S3.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
